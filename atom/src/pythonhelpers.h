ca87e15f07051d963bc1b20065c16bdc020af0fd
Peter Azmanov <peter.azmanov@transas.com>
2015-10-23 19:17:21 +0300

    sortedmap quick fix (enaml 0.9.8 uses unorderable in py3k keys)
    Signed-off-by: Peter Azmanov <peter.azmanov@transas.com>

--- atom/src/pythonhelpers.h
+++ atom/src/pythonhelpers.h
@@ -293,8 +293,23 @@ public:
             return true;
         if( r == 0 )
             return false;
-        if( clear_err && PyErr_Occurred() )
-            PyErr_Clear();
+
+        if ( PyErr_Occurred() )
+        {
+            if( clear_err )
+                PyErr_Clear();
+
+            // FIXME: compare pointers in case of comparison problems
+            switch (opid)
+            {
+            case Py_LT: return m_pyobj < other;
+            case Py_LE: return m_pyobj <= other;
+            case Py_EQ: return m_pyobj == other;
+            case Py_NE: return m_pyobj != other;
+            case Py_GT: return m_pyobj > other;
+            case Py_GE: return m_pyobj >= other;
+            }
+        }
         return false;
     }
 
